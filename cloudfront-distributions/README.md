3b6e4e21eaeabb0339fbfc38368b142c62ceb024
Waldemar Hummer <waldemar.hummer@gmail.com>
2019-11-14 01:20:42 +0100

    add simple CloudFront example

---
+++ cloudfront-distributions/README.md
@@ -0,0 +1,57 @@
+# LocalStack Demo: Serving Files and APIs via CloudFront
+
+Simple demo application illustrating CloudFront distributions running locally using LocalStack.
+
+## Prerequisites
+
+* LocalStack
+* Docker
+* `make`
+* [`awslocal`](https://github.com/localstack/awscli-local)
+
+## Installing
+
+To install the dependencies:
+```
+make install
+```
+
+## App Details
+
+Please refer to the `distconfig.json` file for details about the CloudFront distribution.
+
+## Starting LocalStack
+
+Make sure that LocalStack is started with the following `SERVICES` configuration:
+```
+LOCALSTACK_API_KEY=... DEBUG=1 SERVICES=edge,serverless,ssm,cloudfront localstack start
+```
+
+Please also ensure that your local IP address is registered as a DNS nameserver in your environment (e.g., by configuring `/etc/resolv.conf`).
+
+## Running
+
+Deploy the app locally and run a Lambda test invocation:
+```
+make run
+```
+
+You should some log output of the Serverless deployment, then a success message in the terminal:
+```
+Serverless: Stack update finished...
+...
+Trying to call distribution endpoint (note: it may take a few minutes for the DNS name to become available)
+curl -k https://69e6db0c.cloudfront.net/index
+...
+<html>
+<body>
+...
+```
+
+Finally, the test app should be accessible under the URL from the output above, e.g., https://69e6db0c.cloudfront.net/index .
+
+Please note: It may take some time for the DNS name to propagate and become available in your environment - you may have to retry loading the page in your browser a couple of times.
+
+## License
+
+This code is available under the Apache 2.0 license.
---
+++ cloudfront-distributions/handler.js
@@ -0,0 +1,62 @@
+const AWS = require('aws-sdk');
+
+const cloudFrontUrl = 'https://6cc520ae.cloudfront.net';
+const htmlBucket = 'htmlpages';
+const dataBucket = 'userdata';
+
+const s3Endpoint = `${process.env.LOCALSTACK_HOSTNAME}:4572`;
+const s3Params = {
+  endpoint: s3Endpoint, sslEnabled: false, s3ForcePathStyle: true
+};
+const s3 = new AWS.S3(s3Params);
+
+module.exports.getHtml = async event => {
+  const pathParam = event.pathParameters;
+  const path = pathParam && (pathParam.path || pathParam['path+']);
+  const key = `${path}.html`;
+  const params = {
+    Bucket: htmlBucket,
+    Key: key
+  };
+  const response = await s3.getObject(params).promise();
+  return {
+    statusCode: 200,
+    body: response.Body.toString('utf-8'),
+    headers: {'Content-Type': 'text/html'}
+  };
+};
+
+module.exports.getData = async event => {
+    const key = event.pathParameters.user_id;
+    const params = {
+      Bucket: dataBucket,
+      Key: key
+    };
+    const response = await s3.getObject(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: response.Body.toString('utf-8') })
+    };
+};
+
+module.exports.putData = async event => {
+  const key = event.pathParameters.user_id;
+  const params = {
+    ACL: 'public-read',
+    Body: Math.random().toString().replace('0.',''),
+    Bucket: dataBucket,
+    Key: key,
+    ContentType: 'text/plain'
+  };
+  await s3.upload(params).promise();
+  const signer = new AWS.CloudFront.Signer(process.env.cloudFrontAccessKeyId, process.env.cloudFrontPrivateKey);
+  const resultUrl = signer.getSignedUrl({url: `${cloudFrontUrl}/${key}/data`, expires: Date.now() + 360000})
+  return {
+    statusCode: 201,
+    body: JSON.stringify(
+      {
+        result: resultUrl
+      }
+    ),
+  };
+};
