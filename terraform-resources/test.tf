8b49368f1da62fb222f01923fecd0a446ba18825
Waldemar Hummer <waldemar.hummer@gmail.com>
2019-12-09 16:43:09 +0100

    extend terraform samples

--- terraform-resources/test.tf
+++ terraform-resources/test.tf
@@ -33,3 +33,88 @@ resource "aws_s3_bucket" "testBucket" {
   bucket = "myBucket"
   acl = "private"
 }
+
+resource "aws_api_gateway_authorizer" "demo" {
+  name                   = "demo"
+  rest_api_id            = "${aws_api_gateway_rest_api.demo.id}"
+  authorizer_uri         = "${aws_lambda_function.authorizer.invoke_arn}"
+  authorizer_credentials = "${aws_iam_role.invocation_role.arn}"
+  type = "TOKEN"
+}
+
+resource "aws_api_gateway_rest_api" "demo" {
+  name = "auth-demo"
+}
+
+resource "aws_iam_role" "invocation_role" {
+  name = "api_gateway_auth_invocation"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": {
+    "Effect": "Allow",
+    "Action": "s3:ListBucket",
+    "Resource": "arn:aws:s3:::example_bucket"
+  }
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "invocation_policy" {
+  name = "default"
+  role = "${aws_iam_role.invocation_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": {
+    "Effect": "Allow",
+    "Action": "s3:ListBucket",
+    "Resource": "arn:aws:s3:::example_bucket"
+  }
+}
+EOF
+}
+
+resource "aws_iam_role" "lambda" {
+  name = "demo-lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": {
+    "Effect": "Allow",
+    "Action": "s3:ListBucket",
+    "Resource": "arn:aws:s3:::example_bucket"
+  }
+}
+EOF
+}
+
+resource "aws_lambda_function" "authorizer" {
+  filename      = "lambda.zip"
+  function_name = "api_gateway_authorizer"
+  role          = "${aws_iam_role.lambda.arn}"
+  handler       = "lambda.hello"
+  runtime       = "python3.7"
+  source_code_hash = "${filebase64sha256("lambda.zip")}"
+}
+
+resource "aws_elasticsearch_domain" "example" {
+  domain_name           = "test-domain"
+  elasticsearch_version = "1.5"
+
+  cluster_config {
+    instance_type = "r4.large.elasticsearch"
+  }
+
+  snapshot_options {
+    automated_snapshot_start_hour = 23
+  }
+
+  tags = {
+    Domain = "TestDomain"
+  }
+}
