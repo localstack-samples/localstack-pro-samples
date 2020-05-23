8e5d92c6eb3f05dac40b09026fe34180e7b976c0
Waldemar Hummer <waldemar.hummer@gmail.com>
2020-05-23 11:26:52 +0200

    add simple demo for AWS Transfer

---
+++ transfer-ftp-s3/README.md
@@ -0,0 +1,43 @@
+# LocalStack Demo: FTP Upload to S3 Bucket via AWS Transfer API
+
+Simple demo application illustrating the use of AWS Transfer API in LocalStack.
+
+## Prerequisites
+
+* LocalStack
+* Docker
+* `make`
+
+## Installing
+
+To install the dependencies:
+```
+make install
+```
+
+## Running
+
+Make sure that LocalStack is started with the following `SERVICES` configuration:
+```
+LOCALSTACK_API_KEY=... DEBUG=1 SERVICES=s3,transfer localstack start
+```
+
+The following command runs the test application locally, creates an FTP server via AWS Transfer API locally, uploads two files via FTP, and downloads the files from the target S3 bucket:
+```
+make run
+```
+
+You should then see a couple of log messages in the terminal:
+```
+Running Test: Creating FTP server and uploading files to S3 via Transfer API
+Creating FTP server in AWS Transfer API
+Connecting to AWS Transfer FTP server on local port 4510
+Uploading file to FTP root directory
+Uploading file to FTP sub-directory
+Downloading files from S3 root and sub-directory
+Test done.
+```
+
+## License
+
+This code is available under the Apache 2.0 license.
---
+++ transfer-ftp-s3/test.py
@@ -0,0 +1,96 @@
+import io
+import time
+import uuid
+import boto3
+from ftplib import FTP, FTP_TLS
+
+EDGE_URL = 'http://localhost:4566'
+
+USERNAME = 'user_123'
+BUCKET = 'transfer-files'
+S3_DIR = 'mydir'
+S3_FILENAME = 'test-file-aws-transfer.txt'
+FTP_USER_DEFAULT_PASSWD = '12345'
+FILE_CONTENT = b'title "Test" \nfile content!!'
+
+
+def create_transfer_api():
+    transfer_client, s3_client = get_clients()
+
+    print('Creating FTP server in AWS Transfer API')
+
+    role_arn = 'arn:aws:iam::testrole'
+
+    rs = transfer_client.create_server(
+        EndpointType='PUBLIC',
+        IdentityProviderType='SERVICE_MANAGED',
+        Protocols=['FTP']
+    )
+    time.sleep(1)
+
+    server_id = rs['ServerId']
+    port = int(server_id.split(':')[1])
+
+    s3_client.create_bucket(Bucket=BUCKET)
+
+    transfer_client.create_user(
+        ServerId=server_id,
+        HomeDirectory=BUCKET,
+        HomeDirectoryType='PATH',
+        Role=role_arn,
+        UserName=USERNAME
+    )
+    return server_id, port
+
+
+def upload_files(server_id, ftp_port):
+    transfer_client, s3_client = get_clients()
+
+    ftp = FTP()
+    print('Connecting to AWS Transfer FTP server on local port %s' % ftp_port)
+    ftp.connect('localhost', port=ftp_port)
+
+    # connect to FTP server
+    result = ftp.login(USERNAME, FTP_USER_DEFAULT_PASSWD)
+    assert 'Login successful.' in result
+
+    # upload file to root dir
+    print('Uploading file to FTP root directory')
+    ftp.storbinary('STOR %s' % S3_FILENAME, io.BytesIO(FILE_CONTENT))
+    time.sleep(1)
+
+    # upload file to sub dir
+    print('Uploading file to FTP sub-directory')
+    ftp.mkd(S3_DIR)
+    ftp.cwd(S3_DIR)
+    ftp.storbinary('STOR %s' % S3_FILENAME, io.BytesIO(FILE_CONTENT))
+    time.sleep(1)
+
+    ftp.quit()
+
+
+def download_files(server_id, ftp_port):
+    transfer_client, s3_client = get_clients()
+
+    print('Downloading files from S3 root and sub-directory')
+
+    rs = s3_client.get_object(Bucket=BUCKET, Key=S3_FILENAME)
+    assert rs['Body'].read() == FILE_CONTENT
+
+    rs = s3_client.get_object(Bucket=BUCKET, Key='{}/{}'.format(S3_DIR, S3_FILENAME))
+    assert rs['Body'].read() == FILE_CONTENT
+
+
+def get_clients():
+    return boto3.client('transfer', endpoint_url=EDGE_URL), boto3.client('s3', endpoint_url=EDGE_URL)
+
+
+def main():
+    server_id, ftp_port = create_transfer_api()
+    upload_files(server_id, ftp_port)
+    download_files(server_id, ftp_port)
+    print('Tests succesfully completed.')
+
+
+if __name__ == '__main__':
+    main()
