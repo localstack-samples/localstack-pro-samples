7cb2c3fc0f4e3ef6dfed268eec86ae28cb99420d
Waldemar Hummer <waldemar.hummer@gmail.com>
2022-02-14 11:20:25 +0100

    minor adjustments in run.sh script

--- ec2-custom-ami/run.sh
+++ ec2-custom-ami/run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+export AWS_DEFAULT_REGION=us-east-1
 IMAGE_URL=https://cdn.amazonlinux.com/os-images/2.0.20191217.0/virtualbox/amzn2-virtualbox-2.0.20191217.0-x86_64.xfs.gpt.vdi
-BUCKET=test-bucket
 IMAGE_FILE=amzn2.vdi
 
 test -e $IMAGE_FILE || wget -O $IMAGE_FILE $IMAGE_URL
+
+PK_FILE=$HOME/.ssh/id_rsa
+if [ ! -f $PK_FILE ]; then
+  PK_FILE=$PWD/localstack.id_rsa
+fi
 
 VPC_ID=$(awslocal ec2 create-vpc --cidr-block 0.0.0.0/24 | jq -r '.Vpc.VpcId')
 SUB_ID=$(awslocal ec2 create-subnet --cidr-block 0.0.0.0/26 --vpc-id $VPC_ID | jq -r '.Subnet.SubnetId')
@@ -14,7 +19,8 @@ IMAGE_ID=$(awslocal ec2 import-image --description "Test VM image" --license-typ
 
 perl -i -pe 's/"source_ami": .*/"source_ami": "'$IMAGE_ID'",/' packer.json
 perl -i -pe 's/"subnet_id": .*/"subnet_id": "'$SUB_ID'",/' packer.json
-perl -i -pe 's|"ssh_private_key_file": .*|"ssh_private_key_file": "'$PWD'/localstack.id_rsa",|' packer.json
+perl -i -pe 's|"ssh_private_key_file": .*|"ssh_private_key_file": "'$PK_FILE'",|' packer.json
 
 echo "Starting image creation using packer"
-packer build packer.json || sudo packer build packer.json
+packer build packer.json
+# sudo packer build packer.json
