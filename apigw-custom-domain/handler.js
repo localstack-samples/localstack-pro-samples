2ca6e04b97fb88ead9351697c6b4ace0d5aad688
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2022-02-27 19:35:50 +0000

    fix apigw-custom-domain example

--- apigw-custom-domain/handler.js
+++ apigw-custom-domain/handler.js
@@ -1,7 +1,17 @@
-module.exports.hello = () => {
-  return 'Hello';
+module.exports.hello = (event, context, callback) => {
+		const response = {
+				"statusCode": 200,
+				"headers": {},
+				"body": 'hello world'
+		}
+		callback(null, response);
 };
 
-module.exports.goodbye = () => {
-  return 'Goodbye';
+module.exports.goodbye = (event, context, callback) => {
+		const response = {
+				"statusCode": 200,
+				"headers": {},
+				"body": 'goodbye'
+		}
+		callback(null, response);
 };
