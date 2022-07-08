688ca930e99d9a72a5be27958f11fd9ad2cb8d04
Ben Simon Hartung <42031100+bentsku@users.noreply.github.com>
2022-07-08 12:47:38 +0200

    fix elb-load-balancing - handler return format (#125)

--- elb-load-balancing/handler.js
+++ elb-load-balancing/handler.js
@@ -1,9 +1,27 @@
 module.exports.hello1 = async (event) => {
   console.log(event);
-  return 'Hello 1';
+  return {
+    "isBase64Encoded": false,
+    "statusCode": 200,
+    "statusDescription": "200 OK",
+    "headers": {
+        "Content-Type": "application/json"
+    },
+    "body": "Hello 1"
+  };
 };
 
+// https://docs.aws.amazon.com/elasticloadbalancing/latest/application/lambda-functions.html#respond-to-load-balancer
+// for response format from lambda handler
 module.exports.hello2 = async (event) => {
   console.log(event);
-  return 'Hello 2';
+    return {
+    "isBase64Encoded": false,
+    "statusCode": 200,
+    "statusDescription": "200 OK",
+    "headers": {
+        "Content-Type": "application/json"
+    },
+    "body": "Hello 2"
+  };
 };
