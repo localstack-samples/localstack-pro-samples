a1e5a3b8b85206bf2a0f2e35595b4547494cdcc1
Bernhard Matyas <bernhard.matyas@localstack.cloud>
2022-08-05 14:11:33 +0200

    work in review suggestions
     - remove the extra command from the root Makefile (not needed any longer) - adjust start/stop root targets - remove bash from assert, adjust usage of script accordingly

--- iam-policy-enforcement/run.sh
+++ iam-policy-enforcement/run.sh
@@ -7,7 +7,7 @@ awslocal kinesis create-stream --stream-name s1 --shard-count 1
 echo "Step 2: Trying to create S3 bucket - should get DENIED ..."
 awslocal s3 mb s3://test-iam-bucket
 list_result=$(awslocal s3api list-buckets | jq -r .Buckets)
-../assert "$list_result" == []
+../assert "$list_result" = []
 
 echo "Step 3: Creating user with IAM policy to allow Kinesis access ..."
 pol_arn=$(awslocal iam create-policy --policy-name p1 --policy-document '{"Version":"2012-10-17","Statement":[{"Sid":"pol123","Effect":"Allow","Action":["kinesis:*","s3:*"],"Resource":"*"}]}' | jq -r '.Policy.Arn')
