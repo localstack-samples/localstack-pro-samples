9ccb2b89f828b1fd0d9a7911bf459070a7e17274
Joel Scheuner <joel.scheuner.dev@gmail.com>
2023-02-07 17:21:57 +0100

    Add Lambda PHP/Bref CDK sample

---
+++ lambda-php-bref-cdk-app/backend/minimal_index.php
@@ -0,0 +1,6 @@
+<?php
+// Simplest approach but "Typed PHP Lambda handlers" provide a better development experience
+// https://bref.sh/docs/function/handlers.html
+return function ($event) {
+    return 'Hello World';
+};
