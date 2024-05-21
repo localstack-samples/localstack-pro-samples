44d9377e65c7f72fe62a136b281812eff1f64099
HarshCasper <erbeusgriffincasper@gmail.com>
2024-05-21 11:23:00 +0530

    fix RDS test

--- rds-db-queries/query.py
+++ rds-db-queries/query.py
@@ -1,16 +1,22 @@
 import os
+import time
+
 import boto3
 import psycopg2
 
-RDS_ENDPOINT = os.environ.get('RDS_ENDPOINT') or 'http://localhost:4566'
+RDS_ENDPOINT = os.environ.get("RDS_ENDPOINT") or "http://localhost:4566"
 
 
 def run_queries(instance):
-    print('Run DB queries against RDS instance %s' % instance['DBInstanceIdentifier'])
-    port = instance['Endpoint']['Port']
-    conn = psycopg2.connect("dbname=test user=test password='test' host=localhost port=%s" % port)
+    print("Run DB queries against RDS instance %s" % instance["DBInstanceIdentifier"])
+    port = instance["Endpoint"]["Port"]
+    conn = psycopg2.connect(
+        "dbname=test user=test password='test' host=localhost port=%s" % port
+    )
     with conn.cursor() as cur:
-        cur.execute('CREATE TABLE person ("id" INTEGER, "name" VARCHAR not null, PRIMARY KEY ("id"))')
+        cur.execute(
+            'CREATE TABLE person ("id" INTEGER, "name" VARCHAR not null, PRIMARY KEY ("id"))'
+        )
         cur.execute("INSERT INTO person VALUES (1, 'Jane')")
         cur.execute("INSERT INTO person VALUES (2, 'Alex')")
         cur.execute("INSERT INTO person VALUES (3, 'Maria')")
@@ -21,28 +27,31 @@ def run_queries(instance):
 
 
 def create_db():
-    print('Creating RDS DB instance')
+    print("Creating RDS DB instance")
     client = connect_rds()
-    instance = client.create_db_instance(Engine='postgres', DBInstanceClass='c1', DBInstanceIdentifier='i1')
-    return instance['DBInstance']
+    instance = client.create_db_instance(
+        Engine="postgres", DBInstanceClass="c1", DBInstanceIdentifier="i1"
+    )
+    return instance["DBInstance"]
 
 
 def delete_db(instance):
-    inst_id = instance['DBInstanceIdentifier']
-    print('Deleting RDS DB instance %s' % inst_id)
+    inst_id = instance["DBInstanceIdentifier"]
+    print("Deleting RDS DB instance %s" % inst_id)
     client = connect_rds()
     client.delete_db_instance(DBInstanceIdentifier=inst_id)
 
 
 def connect_rds():
-    return boto3.client('rds', endpoint_url=RDS_ENDPOINT)
+    return boto3.client("rds", endpoint_url=RDS_ENDPOINT)
 
 
 def main():
     instance = create_db()
+    time.sleep(7) # wait for the instance to be ready
     run_queries(instance)
     delete_db(instance)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
