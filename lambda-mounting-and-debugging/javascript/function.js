57e7d4d270e800b04e8b5db67581092d5321172b
Joel Scheuner <joel.scheuner.dev@gmail.com>
2026-02-02 16:02:56 +0100

    Remove outdated Lambda mounting and debugging sample

--- lambda-mounting-and-debugging/javascript/function.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event) => {
-    console.log(event);
-    const response = {
-        statusCode: 200,
-        body: "ok",
-    };
-    return response;
-};
--- lambda-mounting-and-debugging/python/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-awslocal lambda create-function --function-name func1 --timeout 60 --code "S3Bucket=hot-reload,S3Key=$(pwd)/" --handler handler.handler --role arn:aws:iam::000000000000:role/test-role --runtime python3.9
-awslocal lambda invoke --function-name func1 test.lambda.log --payload '{"message":"Hello from LocalStack!"}'
