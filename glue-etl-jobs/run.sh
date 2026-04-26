af202587913cee9020dd5bb45d4ff316a8567a08
HarshCasper <erbeusgriffincasper@gmail.com>
2026-04-26 17:12:35 +0530

    glue-etl-jobs

--- glue-etl-jobs/run.sh
+++ glue-etl-jobs/run.sh
@@ -12,6 +12,7 @@ awslocal s3 cp "job.py" "$S3_URL"
 awslocal s3 mb "s3://glue-sample-target"
 
 awslocal rds create-db-cluster --db-cluster-identifier "$CLUSTER_IDENTIFIER" --engine aurora-postgresql --database-name test
+awslocal rds wait db-cluster-available --db-cluster-identifier "$CLUSTER_IDENTIFIER"
 
 db_port=$(awslocal rds describe-db-clusters --db-cluster-identifier "$CLUSTER_IDENTIFIER" | jq -r '.DBClusters[0].Port')
 echo Using local RDS database on port $db_port ...
@@ -28,15 +29,17 @@ awslocal glue create-connection \
   --connection-input '{"Name": "'$CONNECTION_NAME'", "ConnectionType": "JDBC", "ConnectionProperties": {"USERNAME": "test", "PASSWORD": "test", "JDBC_CONNECTION_URL": "jdbc:postgresql://localhost.localstack.cloud:'$db_port'"}}'
 
 secret=$(awslocal secretsmanager create-secret --name pass --secret-string "test" | jq -r ".ARN")
+db_resource_arn="arn:aws:rds:us-east-1:000000000000:cluster:$CLUSTER_IDENTIFIER"
 
 echo Creating Postgres database tables with data ...
-awslocal rds-data execute-statement --resource-arn arn:aws:rds:us-east-1:000000000000:cluster:$CLUSTER_IDENTIFIER --secret-arn $secret --sql 'CREATE TABLE IF NOT EXISTS persons(id varchar, name varchar)'
-awslocal rds-data execute-statement --resource-arn arn:aws:rds:us-east-1:000000000000:cluster:$CLUSTER_IDENTIFIER --secret-arn $secret --sql 'CREATE TABLE IF NOT EXISTS organizations(org_id varchar, org_name varchar)'
-awslocal rds-data execute-statement --resource-arn arn:aws:rds:us-east-1:000000000000:cluster:$CLUSTER_IDENTIFIER --secret-arn $secret --sql 'CREATE TABLE IF NOT EXISTS memberships(person_id varchar, organization_id varchar)'
-awslocal rds-data execute-statement --resource-arn arn:aws:rds:us-east-1:000000000000:cluster:$CLUSTER_IDENTIFIER --secret-arn $secret --sql "insert into persons(id, name) VALUES('p1', 'person 1')"
-awslocal rds-data execute-statement --resource-arn arn:aws:rds:us-east-1:000000000000:cluster:$CLUSTER_IDENTIFIER --secret-arn $secret --sql "insert into organizations(org_id, org_name) VALUES('o1', 'org1')"
-awslocal rds-data execute-statement --resource-arn arn:aws:rds:us-east-1:000000000000:cluster:$CLUSTER_IDENTIFIER --secret-arn $secret --sql "insert into memberships(person_id, organization_id) VALUES('p1', 'o1')"
-awslocal rds-data execute-statement --resource-arn arn:aws:rds:us-east-1:000000000000:cluster:$CLUSTER_IDENTIFIER --secret-arn $secret --sql 'CREATE TABLE IF NOT EXISTS hist_root(id varchar, name varchar, org_id varchar, org_name varchar, person_id varchar, organization_id varchar)'
+awslocal rds-data execute-statement --resource-arn "$db_resource_arn" --secret-arn "$secret" --database postgres --sql 'CREATE DATABASE test' || true
+awslocal rds-data execute-statement --resource-arn "$db_resource_arn" --secret-arn "$secret" --database test --sql 'CREATE TABLE IF NOT EXISTS persons(id varchar, name varchar)'
+awslocal rds-data execute-statement --resource-arn "$db_resource_arn" --secret-arn "$secret" --database test --sql 'CREATE TABLE IF NOT EXISTS organizations(org_id varchar, org_name varchar)'
+awslocal rds-data execute-statement --resource-arn "$db_resource_arn" --secret-arn "$secret" --database test --sql 'CREATE TABLE IF NOT EXISTS memberships(person_id varchar, organization_id varchar)'
+awslocal rds-data execute-statement --resource-arn "$db_resource_arn" --secret-arn "$secret" --database test --sql "insert into persons(id, name) VALUES('p1', 'person 1')"
+awslocal rds-data execute-statement --resource-arn "$db_resource_arn" --secret-arn "$secret" --database test --sql "insert into organizations(org_id, org_name) VALUES('o1', 'org1')"
+awslocal rds-data execute-statement --resource-arn "$db_resource_arn" --secret-arn "$secret" --database test --sql "insert into memberships(person_id, organization_id) VALUES('p1', 'o1')"
+awslocal rds-data execute-statement --resource-arn "$db_resource_arn" --secret-arn "$secret" --database test --sql 'CREATE TABLE IF NOT EXISTS hist_root(id varchar, name varchar, org_id varchar, org_name varchar, person_id varchar, organization_id varchar)'
 
 echo Starting Glue job from PySpark script ...
 awslocal glue create-job --name $JOB_NAME --role r1 \
