49ae9280d9a2066c770d6a3a4383e486126a028c
Brian Rinaldi <brian.rinaldi@gmail.com>
2026-09-21 07:35:43 -0400

    Add AWS PRM partner tag (aws-apn-id)
    Tag-only change, no functional impact.

--- cdk-resources/bin/cdk-resources-1.ts
+++ cdk-resources/bin/cdk-resources-1.ts
@@ -3,4 +3,5 @@ import * as cdk from '@aws-cdk/core';
 import { CdkResources1Stack } from '../lib/cdk-resources-1-stack';
 
 const app = new cdk.App();
-new CdkResources1Stack(app, 'CdkResources1Stack');
+const cdkResources1Stack = new CdkResources1Stack(app, 'CdkResources1Stack');
+cdk.Tags.of(cdkResources1Stack).add('aws-apn-id', 'pc:9yq38ki5jw5mas7jhjthpgveo');
